b172ba1b399a0655870e5cb537da7c4dcaaa45d3
Jeong Hoon <gbg9609@naver.com>
2024-10-18 08:52:06 +0900

    API Routes, 스타일링

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -7,7 +7,9 @@ type Data = {
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>,
+  res: NextApiResponse<Data>
 ) {
   res.status(200).json({ name: "John Doe" });
 }
+
+// 웹페이지가 아닌 api 라우트로서(api 응답을 정의하는 파일) 자동으로 설정
